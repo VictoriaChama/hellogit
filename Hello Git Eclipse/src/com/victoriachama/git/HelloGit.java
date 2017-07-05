edc66bc732f27d733729387a17e366640237c01b
Victoria <victoriachama@gmail.com>
2017-07-05 15:51:15 +0200

    Committed Hello Git Eclipse

---
+++ Hello Git Eclipse/src/com/victoriachama/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.victoriachama.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Added commit
+		System.out.println("Hello Git!!!");
+
+	}
+
+}
